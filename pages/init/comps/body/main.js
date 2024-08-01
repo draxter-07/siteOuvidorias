1bd7af29d7e21afe6f5571a7bfa3f13729c07762
Philippe <philippe.idalgoprestes@gmail.com>
2024-08-01 16:47:37 -0300

    carrossel adde

--- pages/init/comps/body/main.js
+++ pages/init/comps/body/main.js
@@ -1,6 +1,13 @@
 export default function bodyDiv(){
     return(`
         <div class="bodyDiv">
+            <div class="carrossel">
+                <div class="carrosseis">
+                    <div class="carrInfo">Info1</div>
+                    <div class="carrInfo">Info2</div>
+                    <div class="carrInfo">Info3</div>
+                </div>
+            </div>
             <div class="info">Info</div>
             <div class="info">Info</div>
             <div class="info">Info</div>
--- pages/init/main.js
+++ pages/init/main.js
@@ -1,10 +1,26 @@
 import bodyDiv from "./comps/body/main.js"
 import topoDiv from "./comps/top/main.js"
-function initPageAddFunctionsToButtons(){
+async function initPageAddFunctionsToButtons(){
+    let i = 0;
+    while(true){
+        const carr = document.getElementsByClassName("carrosseis")[0]
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        i += 1
+        if(i == 1){
+            carr.style.translate = "-33%"
+        }
+        else if(i == 2){
+            carr.style.translate = "-66%"
+        }
+        else if(i == 3){
+            carr.style.translate = "0%"
+            i = 0
+        }
+    }
 }
 
-export default function initMain(){
+export default async function initMain(){
     const root = document.getElementById("root")
     root.innerHTML = topoDiv() + bodyDiv()
-    initPageAddFunctionsToButtons()
+    await initPageAddFunctionsToButtons()
 }
